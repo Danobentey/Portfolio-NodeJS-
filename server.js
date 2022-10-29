bd2f5b1622187f025aa4d71c7a6e761e895be574
unknown <danobentey@gmail.com>
2022-10-29 22:44:32 +0100

    adds projects and services pages

---
+++ server.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const fs = require('fs');
+
+
+
+const server =  http.createServer((req, res) => {
+  console.log ('Server started');
+  console.log(req.url, req.method)
+  
+  res.setHeader('Content-type', 'text/html');
+  
+  let path = './views/';
+  //  if (req.url = '/') {
+  //   path += 'index.html'
+  //  } else {
+    
+  //  }
+    
+    switch (req.url) {
+      case '/':
+        path += 'index.html';
+        break;
+      
+      case '/index.html':
+        path += 'index.html';
+        break;
+        
+      case '/about.html':
+        path += 'about.html';
+        break;
+      
+      case '/contact.html':
+        path += 'contact.html'
+        break
+
+        case '/projects.html':
+          path += 'projects.html'
+          break
+
+          case '/services.html':
+            path += 'services.html'
+            break 
+
+      default: path += '404.html'
+        break;
+      }
+      
+      console.log(path, typeof(path))
+      
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err)
+      res.end()
+    } else {
+      res.end(data)
+      console.log(path, typeof(path))
+    }
+  });
+
+})
+
+server.listen(2000, 'localhost', () => {
+  console.log('listening for request')
+})
